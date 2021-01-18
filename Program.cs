f5614fa107f4976c8043837018552bded7249f15
Сергей Захаров <sergei86zahar@mail.ru>
2021-01-18 11:17:24 +0700

    1

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Data.Odbc;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 
@@ -22,7 +23,9 @@ namespace FTP_Attestation_CopyFiles
         public static FTPConnection ftp;
 
         public static string connectionString;
-        public static string command;
+        public static DateTime firstDate;                 // Дата создания директории
+        public static DateTime lastDate;                  // Дата для базы данных
+        public static List<string> part;
 
         public static void log(string message) // запись логов в текстовый файл
         {
@@ -33,7 +36,7 @@ namespace FTP_Attestation_CopyFiles
         }
         public static void InsertRow(string connectionString, DateTime dateTime) // соединение с базой данных по ODBC
         {
-            string query = "select part_id from incube.dbo.tb_part where end_time>'2020-11-24 12:00:00'";
+            string query = $"select part_id from incube.dbo.tb_part where end_time>'{dateTime}'";
 
             using (OdbcConnection connection = new OdbcConnection(connectionString))
             {
@@ -41,58 +44,38 @@ namespace FTP_Attestation_CopyFiles
                 try
                 {
                     connection.Open();
-                    List<string> part = new List<string>();
+                    Console.WriteLine("Соединение с базой данных установлено");
+                    part = new List<string>();
                     // Запустите DataReader и получите доступ к данным.
 
                     OdbcDataReader reader = command.ExecuteReader();
                     while (reader.Read())
                     {
+                        //part[i] = reader[i].ToString();
                         part.Add(reader[0].ToString());
+                        Console.WriteLine("Получили список из базы данных");
                     }
-                    foreach(string pp in part)
+                    /*foreach(string pp in part)
                     {
 
                         Console.WriteLine(pp);
-                    }
+                    }*/
                     
-                    DateTime dateTime1 = DateTime.Now;
+                    //DateTime dateTime1 = DateTime.Now;
                     // убираем букву Z из конца строки даты
-                    string dat = (dateTime1.ToUniversalTime().ToString("u")).Replace("Z", ""); 
-                    Console.WriteLine(dat);
-                    Console.Read();
+                    //string dat = (dateTime1.ToUniversalTime().ToString("u")).Replace("Z", ""); 
+                    //Console.WriteLine(dat);
+                    //Console.Read();
                     // Call Close when done reading.
                     reader.Close();
                 }
                 catch (Exception ss)
                 {
-                    log(ss.ToString());
+                    log(ss.ToString()+" Чтение из базы");
                 }
             }
         }
-        /*static private string[] InsertRow(string connectionString, string queryString, DateTime dateTime) // соединение с базой данных по ODBC
-        {
-            try
-            {
-                OdbcCommand command = new OdbcCommand(queryString);
-
-                using (OdbcConnection connection = new OdbcConnection(connectionString))
-                {
-                    command.Connection = connection;
-                    connection.Open();
-                    //command.ExecuteNonQuery();
-                    command.CommandType = System.Data.CommandType.Text;
-                    command.CommandText = "";
-                    command.Parameters.Add("", OdbcType.DateTime).Value = dateTime;
-                    string[] guids = command.ExecuteScalar().ToString();
-                    Console.WriteLine(guids);
-                    return guids;
-                }
-            }
-            catch (Exception a)
-            {
-                log(a.ToString());
-            }
-        }*/
+       
         static void Main(string[] args)
         {
             Local_long_listFiles = new List<string>();
@@ -167,31 +150,24 @@ namespace FTP_Attestation_CopyFiles
                 ftp.UserName = userName;
                 ftp.Password = password;
                 ftp.Connect();
+                Console.WriteLine("Соединение с FTP установлено");
             }
             catch (Exception a)
             {
-                log(a.ToString());
+                log(a.ToString()+" Чтение конфига");
             }
 
-            try
-            {
-                InsertRow(connectionString, new DateTime());
-            }
-            catch (Exception s)
-            {
-                log(s.ToString());
-            }
 
             // Получение списка директорий на сервере ////////////////////////////////////////////////////////////////////////////////////////////////
-            /*FTPFile[] GUID_directory = ftp.GetFileInfos();  
-            
+            FTPFile[] GUID_directory = ftp.GetFileInfos();
+
             // сортировка директории по дате создания (первая самая новая)
             FTPFile temp;
             for (int i = 0; i < GUID_directory.Length - 1; i++)
             {
                 for (int j = i + 1; j < GUID_directory.Length; j++)
                 {
-                    Console.WriteLine(GUID_directory[j].LastModified);
+                    //Console.WriteLine(GUID_directory[j].LastModified);
                     if (GUID_directory[i].LastModified < GUID_directory[j].LastModified)
                     {
                         temp = GUID_directory[i];
@@ -200,54 +176,169 @@ namespace FTP_Attestation_CopyFiles
                     }
                 }
             }
-            Console.WriteLine("После сортировки");
-            foreach (FTPFile a in GUID_directory)
-            {
-                Console.WriteLine(a.Name + " - " + a.LastModified);
-            }
+            /* Console.WriteLine("После сортировки");
+             foreach (FTPFile a in GUID_directory)
+             {
+                 Console.WriteLine(a.Name + " - " + a.LastModified);
+             }*/
+
+            firstDate = GUID_directory[0].LastModified;
+            lastDate = firstDate.AddHours(5);
+            Console.WriteLine("Получили дату для выборки из базы данных");
+
             // Копирование и удаление файлов и директорий ////////////////////////////////////////////////////////////////////////////////////////////
-            if (GUID_directory.Length > 1)
+            if (GUID_directory.Length > 0)
             {
-                for (int i = 1; i < GUID_directory.Length; i++)
+                try
                 {
-                    // создаем папку на вычислителе с именем партии
-                    DirectoryInfo dirInfo = new DirectoryInfo(dir_Name + "/" + GUID_directory[i].Name);   
-                    if (!dirInfo.Exists)
-                    {
-                        dirInfo.Create();
-                    }
-                    ftp.ChangeWorkingDirectory(GUID_directory[i].Name);     // сменить рабочую директорию, войти в папку партии
-                    FTPFile[] Into_GUID = ftp.GetFileInfos();               // файлы в папке партии
-                    for (int k = 0; k < Into_GUID.Length; k++)
-                    {
-                        if (Into_GUID[k].Dir)  // true если это директория
-                        {
-                            dirInfo.CreateSubdirectory(Into_GUID[k].Name);             // создаем папку на вычислителе с номером вагона
-                            ftp.ChangeWorkingDirectory(Into_GUID[k].Name);
-                            FTPFile[] files = ftp.GetFileInfos();                      // файлы в папке вагона
-                            foreach(FTPFile a in files)
-                            {
-                                // копирование файла с сервера на компьютер
-                                ftp.DownloadFile(dir_Name + "/" + GUID_directory[i].Name + "/" + Into_GUID[k].Name + "/" + a.Name, a.Name);
-                                ftp.DeleteFile(a.Name);
-                            }
-                            ftp.ChangeWorkingDirectoryUp();                    // вернуться из папки
-                            ftp.DeleteDirectory(Into_GUID[k].Name);
-                        }
-                        else
-                        {
-                            ftp.DownloadFile(dir_Name + "/" + GUID_directory[i].Name + "/" + Into_GUID[k].Name, Into_GUID[k].Name);
-                            ftp.DeleteFile(Into_GUID[k].Name);
-                        }
-                    }
-                    ftp.ChangeWorkingDirectoryUp();                    // вернуться из папки
-                    ftp.DeleteDirectory(GUID_directory[i].Name);            // удалить папку
-                    Console.WriteLine("Директория удалена");
+                    InsertRow(connectionString, lastDate); // получаем список партий из базы
                 }
-                Console.WriteLine("Старые директории удалены");
+                catch (Exception s)
+                {
+                    log(s.ToString()+ " Получение списка партий из базы");
+                }
+                for (int i = 0; i < GUID_directory.Length; i++)
+                {
+                    foreach (string pr in part)
+                    {
+                        try
+                        {
+                            if (pr == GUID_directory[i].Name)   // если совпадает номер на сервере и в базе (значить партия закрыта)
+                            {
+                                // создаем папку на вычислителе с именем партии
+                                DirectoryInfo dirInfo = new DirectoryInfo(dir_Name + "/" + GUID_directory[i].Name);
+                                if (!dirInfo.Exists)
+                                {
+                                    dirInfo.Create();
+                                    Console.WriteLine($"Создали папки партии {GUID_directory[i].Name} на жестком диске");
+                                }
+                                ftp.ChangeWorkingDirectory(GUID_directory[i].Name);     // сменить рабочую директорию, войти в папку партии
+                                FTPFile[] Into_GUID = ftp.GetFileInfos();               // файлы в папке партии
+                                if (Into_GUID.Length > 0)
+                                {
+                                    for (int k = 0; k < Into_GUID.Length; k++)
+                                    {
+                                        if (Into_GUID[k].Dir)  // true если это директория
+                                        {
+                                            dirInfo.CreateSubdirectory(Into_GUID[k].Name);             // создаем папку на вычислителе с номером вагона
+                                            ftp.ChangeWorkingDirectory(Into_GUID[k].Name);             // сменить рабочую директорию, войти в папку вагона
+                                            FTPFile[] files = ftp.GetFileInfos();                      // файлы в папке вагона
+                                            if (files.Length > 0)
+                                            {
+                                                foreach (FTPFile a in files)
+                                                {
+                                                    try
+                                                    {
+                                                        // копирование файла с сервера на компьютер
+                                                        ftp.DownloadFile(dir_Name + "/" + GUID_directory[i].Name + "/" + Into_GUID[k].Name + "/" + a.Name, a.Name);
+                                                        Console.WriteLine($"Файл с фото {a.Name} скопирован");
+                                                        // сравниваем размер скаченного файла на диске и сервере
+                                                        FileInfo info = new FileInfo(dir_Name + "/" + GUID_directory[i].Name + "/" + Into_GUID[k].Name + "/" + a.Name);
+                                                        if (info.Length == a.Size)
+                                                        {
+                                                            ftp.DeleteFile(a.Name);                // удаляем файл
+                                                            Console.WriteLine("Фото удалено с сервера");
+                                                        }
+                                                        else
+                                                        {
+                                                            Console.WriteLine($"Не удалось удалить фото {a.Name} после копирования");
+                                                        }
+                                                    }
+                                                    catch (Exception kk)
+                                                    {
+                                                        log(kk.ToString()+" Копирование фотографий");
+                                                    }
+                                                }
+                                                // получаем еще раз список файлов
+                                                FTPFile[] files_last = ftp.GetFileInfos();
+                                                if (files_last.Length == 0)
+                                                {
+                                                    ftp.ChangeWorkingDirectoryUp();                // вернуться из папки
+                                                    try
+                                                    {
+                                                        ftp.DeleteDirectory(Into_GUID[k].Name);        // удалить папку вагона
+                                                        Console.WriteLine("Удалена папка с номером вагона");
+                                                    }
+                                                    catch (Exception jj)
+                                                    {
+                                                        Console.WriteLine("Не удалось удалить папку с номером вагона");
+                                                        log(jj.ToString()+" Удаление папки с номером вагона");
+                                                    }
+                                                }
+                                                else
+                                                {
+                                                    Console.WriteLine("Не удалось удалить папку с номером вагона");
+                                                    ftp.ChangeWorkingDirectoryUp();                // вернуться из папки
+                                                }
+                                            }
+                                        }
+                                        else
+                                        {
+                                            try
+                                            {
+                                                // копируем файл с видео
+                                                ftp.DownloadFile(dir_Name + "/" + GUID_directory[i].Name + "/" + Into_GUID[k].Name, Into_GUID[k].Name);
+                                                Console.WriteLine($"Файл с видео {Into_GUID[k].Name} скопирован");
+                                                FileInfo info = new FileInfo(dir_Name + "/" + GUID_directory[i].Name + "/" + Into_GUID[k].Name);
+                                                // сравниваем размер
+                                                if (info.Length == Into_GUID[k].Size)
+                                                {
+                                                    ftp.DeleteFile(Into_GUID[k].Name);        // удаляем файл
+                                                    Console.WriteLine($"Удалили файл видео {Into_GUID[k].Name} с сервера");
+                                                }
+                                                else
+                                                {
+                                                    Console.WriteLine($"Не удалось удалить файл видео {Into_GUID[k].Name} с сервера");
+                                                }
+                                            }
+                                            catch (Exception jj)
+                                            {
+                                                Console.WriteLine("Ошибка копирования видео");
+                                                log(jj.ToString()+" Копирование видео");
+                                            }
+                                        }
+                                    }
+                                    FTPFile[] Into_GUID_last = ftp.GetFileInfos();            // файлы в папке партии еще раз
+                                    if (Into_GUID_last.Length == 0)
+                                    {
+                                        ftp.ChangeWorkingDirectoryUp();                       // вернуться из папки партии
+                                        try
+                                        {
+                                            ftp.DeleteDirectory(GUID_directory[i].Name);      // удалить папку партии
+                                            Console.WriteLine("Папка партии удалена с сервера");
+                                        }
+                                        catch (Exception jj)
+                                        {
+                                            Console.WriteLine("Ошибка удаления папки партии с сервера");
+                                            log(jj.ToString());
+                                        }
+                                    }
+                                    //Console.WriteLine("Директория удалена");
+                                }
+                                Console.WriteLine("Все доступные партии проработаны");
+                            }
+                        }
+                        catch (Exception dd)
+                        {
+                            Console.WriteLine("Ошибка в процессе копирования и удаления");
+                            log(dd.ToString() + " Ошибка в процессе копирования и удаления");
+                        }
+                    }
+                }
+                //Console.WriteLine("Старые директории удалены");
             }
-            ftp.Close();
-            Console.Read();*/
+            try
+            {
+                ftp.Close();
+                Console.WriteLine("Соединение с FTP успешно закрыто");
+            }
+            catch (Exception ff)
+            {
+                Console.WriteLine("Ошибка закрытия соединения FTP");
+                log(ff.ToString()+ " Ошибка закрытия соединения FTP");
+            }
+            //Console.Read();
+            Thread.Sleep(20000);
         }
     }
 }
